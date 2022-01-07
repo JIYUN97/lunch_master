f84e44ede179551fde68d1e927f9afb28b979971
player31 <keumks0331@gmail.com>
2022-01-07 20:20:49 +0900

    [ADD] userService create and test

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,5 +1,10 @@
+import { UserDto } from './user.dto';
+import { User } from './user.model';
 import { UserRepository } from './user.repository';
 
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
+  create(input: UserDto): Promise<User> {
+    return this.userRepository.create(input);
+  }
 }
