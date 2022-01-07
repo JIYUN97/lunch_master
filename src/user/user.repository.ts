82030fac69ca25eadb820b5e47d24efcb681c3af
player31 <keumks0331@gmail.com>
2022-01-07 14:26:55 +0900

    [ADD] userRepository

---
+++ src/user/user.repository.ts
@@ -0,0 +1,18 @@
+import { UserDto } from './user.dto';
+import UserModel, { User } from './user.model';
+
+export class UserRepository {
+  private readonly repository;
+  constructor() {
+    this.repository = UserModel;
+  }
+
+  async create(input: UserDto): Promise<User> {
+    return this.repository.create(input);
+  }
+
+  async findById(id: number): Promise<User | undefined> {
+    const user = await this.repository.findById(id);
+    return user ?? undefined;
+  }
+}
