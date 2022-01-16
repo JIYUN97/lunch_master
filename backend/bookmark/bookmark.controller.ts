c33a47f4ef9b037d6f90e75ac2326a1844aa9721
YOOJIYUN <ys7638@naver.com>
2022-01-16 20:20:31 +0900

    ADD: 슬랙 전송 기능 초안

--- backend/bookmark/bookmark.controller.ts
+++ backend/bookmark/bookmark.controller.ts
@@ -1,8 +1,9 @@
-import { Router, Request, Response } from 'express';
+import { Router, Request, Response, response } from 'express';
 import { validationMiddleware } from '../middleware';
 import { BookmarkDto } from './bookmark.dto';
 import { BookmarkService } from './bookmark.service';
 import axios from 'axios';
+import qs from 'qs';
 
 export class BookmarkController {
   public router: Router;
@@ -13,6 +14,7 @@ export class BookmarkController {
   }
 
   private initRouter(): void {
+    this.router.post(`${this.path}/slack`, this.postSlack.bind(this));
     this.router.get(`${this.path}/random`, this.getRandomRestaurant.bind(this));
     this.router.post(
       this.path,
@@ -47,13 +49,41 @@ export class BookmarkController {
       'X-Naver-Client-Secret': process.env.naverPassword || '',
     };
     const keyword = (req.query.keyword as string) || '';
-    console.log(keyword);
 
     const url =
       'https://openapi.naver.com/v1/search/local.json?query=' +
       encodeURI(keyword) +
       '&display=5&start=1&sort=random';
     const { data } = await axios.get(url, { headers });
-    return res.send({ success: 'ok', data: data.items });
+    if (!data.items.length) {
+      return res.send({ success: false, msg: '맛집 검색에 실패하였습니다.' });
+    }
+    const restaurant = data.items;
+    const randIndex = Math.floor(Math.random() * restaurant.length);
+    return res.send({ success: 'ok', data: restaurant[randIndex] });
+  }
+
+  async postSlack(
+    req: Request,
+    res: Response,
+  ): Promise<Response<any, Record<string, any>>> {
+    // JWT 토큰으로 로그인 여부 검사, 그 후로 로그인 정보 이용해서 토큰 및 채널 아이디 정보 확인
+    const token = process.env.slack_token;
+    const channel = process.env.channel_id;
+    const { title, roadAddress } = req.body;
+    const data = {
+      'Content-Type': 'application/json;charset=UTF-8',
+      token: token,
+      channel: channel,
+      text: `오늘의 점심!💖 ${title}에서 어때요? \n 주소는 바로 여기 -[${roadAddress}]📌`,
+    };
+    const result = await axios.post(
+      'https://slack.com/api/chat.postMessage',
+      qs.stringify(data),
+    );
+    if (!result.data.ok) {
+      return res.send({ success: false, msg: '슬랙 전송에 실패하였습니다.' });
+    }
+    return res.send({ success: true, msg: '슬랙에 전송하였습니다.' });
   }
 }
